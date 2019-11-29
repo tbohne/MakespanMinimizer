f70ccb4a7047a9ed1d75213d813fb38d30417cb8
tbohne <tbohne@uos.de>
2019-11-29 23:16:49 +0100

    implemented basic job-swap instead of shift

--- src/SwapOperator.java
+++ src/SwapOperator.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Random;
 
 public class ShiftOperator {
@@ -10,7 +11,7 @@ public class ShiftOperator {
         return r.nextInt((max - min) + 1) + min;
     }
 
-    public static Solution generateShiftNeighbor(Solution currSol) {
+    public static Solution generateShiftNeighbor(Solution currSol, List<Shift> performedShifts) {
 
         // select random machine m
         // select random job j assigned to m
@@ -18,25 +19,40 @@ public class ShiftOperator {
         // assign j to z
 
         int machineToShiftJobFrom = getRandomNumberInRange(0, currSol.getMachineAllocations().size() - 1);
-        System.out.println("machine to shift job from: " + machineToShiftJobFrom);
+//        System.out.println("machine to shift job from: " + machineToShiftJobFrom);
         // machine has no jobs
         if (currSol.getMachineAllocations().get(machineToShiftJobFrom).getJobs().size() == 0) { return currSol; }
         int randomJobIdx = getRandomNumberInRange(0, currSol.getMachineAllocations().get(machineToShiftJobFrom).getJobs().size() - 1);
         int job = currSol.getMachineAllocations().get(machineToShiftJobFrom).getJobs().get(randomJobIdx);
-        System.out.println("job to be shifted: " + job);
+//        System.out.println("job to be shifted: " + job);
         currSol.getMachineAllocations().get(machineToShiftJobFrom).getJobs().remove(randomJobIdx);
 
         int targetMachine = getRandomNumberInRange(0, currSol.getMachineAllocations().size() - 1);
         while (targetMachine == machineToShiftJobFrom) {
             targetMachine = getRandomNumberInRange(0, currSol.getMachineAllocations().size() - 1);
         }
-        System.out.println("target machine: " + targetMachine);
+
+        Shift shiftOne = new Shift(job, targetMachine);
+//        System.out.println("target machine: " + targetMachine);
 
         currSol.getMachineAllocations().get(targetMachine).getJobs().add(job);
 
-        System.out.println("#########################");
-        System.out.println(currSol);
-        System.out.println("#########################");
+        if (currSol.getMachineAllocations().get(targetMachine).getJobs().size() == 0) { return currSol; }
+        int randomJobIdx2 = getRandomNumberInRange(0, currSol.getMachineAllocations().get(targetMachine).getJobs().size() - 1);
+        int job2 = currSol.getMachineAllocations().get(targetMachine).getJobs().get(randomJobIdx2);
+//        System.out.println("job to be shifted: " + job);
+        currSol.getMachineAllocations().get(targetMachine).getJobs().remove(randomJobIdx2);
+
+        currSol.getMachineAllocations().get(machineToShiftJobFrom).getJobs().add(job2);
+
+        Shift shiftTwo = new Shift(job2, machineToShiftJobFrom);
+
+        performedShifts.add(shiftOne);
+        performedShifts.add(shiftTwo);
+
+//        System.out.println("#########################");
+//        System.out.println(currSol);
+//        System.out.println("#########################");
 
         return currSol;
     }
