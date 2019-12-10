e4d4fba2f03c7be9b851a51920a19a44e31e8dce
tbohne <tbohne@uos.de>
2019-12-10 22:59:08 +0100

    iterating over all instances / writing solutions to disk

--- src/Test.java
+++ src/Test.java
@@ -1,25 +1,45 @@
+import java.io.File;
+import java.util.Arrays;
+
 public class Test {
+
+    private static final String INSTANCE_PREFIX = "res/instances/";
+    private static final String SOLUTION_PREFIX = "res/solutions/";
 
     public static void main(String[] args) {
 
-        System.out.println("##############################################################");
-        Instance instance = InstanceReader.readInstance("res/instances/PCMAX_U_1_0500_25_4.txt");
-        System.out.println("machines: " + instance.getNumOfMachines());
-        System.out.println("jobs: " + instance.getNumOfJobs());
-        for (int processingTime : instance.getProcessingTimes()) {
-            System.out.print(processingTime + " ");
-        }
-        System.out.println();
-        System.out.println("##############################################################");
+        File dir = new File(INSTANCE_PREFIX);
+        File[] directoryListing = dir.listFiles();
+        assert directoryListing != null;
+        Arrays.sort(directoryListing);
 
-        Solution sol = HeuristicSolver.solve(instance);
+        for (File file : directoryListing) {
 
-        if (sol.isFeasible()) {
-            System.out.println("final: " + sol.getMakespan());
+            String instanceName = file.toString().replace("res/instances/", "").replace(".txt", "");
+            System.out.println("working on: " + instanceName);
+            Instance instance = InstanceReader.readInstance(INSTANCE_PREFIX + instanceName + ".txt");
+            String solutionName = instanceName.replace("instance", "sol");
+
+
+            // TODO: could be toString() for instance
+//            System.out.println("machines: " + instance.getNumOfMachines());
+//            System.out.println("jobs: " + instance.getNumOfJobs());
+//            for (int processingTime : instance.getProcessingTimes()) {
+//                System.out.print(processingTime + " ");
+//            }
+
+            Solution sol = HeuristicSolver.solve(instance);
+
+            if (sol.isFeasible()) {
+                System.out.println("final: " + sol.getMakespan());
 //        System.out.println(sol);
-            SolutionWriter.writeSolution("res/solutions/4.txt", sol);
-        } else {
-            System.out.println("generated infeasible solution..");
+                SolutionWriter.writeSolution(SOLUTION_PREFIX + solutionName + ".txt", sol);
+            } else {
+                System.out.println("generated infeasible solution..");
+                System.out.println(sol);
+                System.exit(0);
+            }
+
         }
     }
 }
