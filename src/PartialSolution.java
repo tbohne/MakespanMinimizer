f89bf6af48d097f91705de270e97e885ba1d0e1e
tbohne <tbohne@uos.de>
2019-12-10 22:28:18 +0100

    implemented representation for partial solutions

---
+++ src/PartialSolution.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PartialSolution implements Comparable<PartialSolution> {
+
+    // r-th partial solution
+    // contains a list where each entry corresponds to a list representing
+    // one machine and containing the jobs performed by that machine
+
+    private List<Machine> machineAllocations;
+
+    public PartialSolution(int numOfMachines) {
+        this.machineAllocations = new ArrayList<>();
+        for (int i = 0; i < numOfMachines; i++) {
+            this.machineAllocations.add(new Machine());
+        }
+    }
+
+    public List<Machine> getMachineAllocations() {
+        return this.machineAllocations;
+    }
+
+    public int computeGap() {
+        return Math.abs(Collections.max(this.machineAllocations).getProcessingTime() - Collections.min(this.machineAllocations).getProcessingTime());
+    }
+
+    public boolean isEmpty() {
+        for (Machine machine : this.machineAllocations) {
+            if (!machine.toString().contains("-")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int compareTo(PartialSolution other) {
+        if (other.computeGap() < this.computeGap()) {
+            return 1;
+        } else if (other.computeGap() > this.computeGap()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        for (Machine machine : this.machineAllocations) {
+            str += machine.toString();
+        }
+        return str;
+    }
+}
