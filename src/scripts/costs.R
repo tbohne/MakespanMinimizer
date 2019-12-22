be8296f6e0dde9531f8792bc2e449223e9ac35cf
tbohne <tbohne@uos.de>
2019-12-22 13:12:37 +0100

    refined scripts

--- src/scripts/costs.R
+++ src/scripts/costs.R
@@ -1,11 +1,10 @@
 library(ggplot2)
 
-input <- read.csv(file = "res/solutions/solutions.csv", header = TRUE, sep = ",")
-solverEntries <- subset(input, solver == "LPT" | solver == "SPS")
+input <- read.csv(file = "res/solutions/S77_solutions.csv", header = TRUE, sep = ",")
+solverEntries <- subset(input, solver == "LPT" | solver == "SPS" | solver == "CPLEX")
 
 plotPointsPre <- ggplot(data = solverEntries, aes(x = obj, y = instance, color = solver, group = solver))
-scaledPlot <- plotPointsPre + geom_point() + xlab("costs") + ylab("instance")
-finalPlot <- scaledPlot #+ scale_color_manual(objues=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
+finalPlot <- plotPointsPre + geom_point() + xlab("costs") + ylab("instance")
 
 ggsave(finalPlot, file = "solver_instance_cost.png", width=6, height=4)
 
@@ -16,3 +15,7 @@ paste("avg costs of LPT: ", round(mean(as.numeric(as.character(LPTCosts[["obj"]]
 SPSData <- subset(input, solver == "SPS")
 SPSCosts <- subset(SPSData, select = c(obj))
 paste("avg costs of SPS: ", round(mean(as.numeric(as.character(SPSCosts[["obj"]]))), digits = 2))
+
+CPLEXData <- subset(input, solver == "SPS")
+CPLEXCosts <- subset(CPLEXData, select = c(obj))
+paste("avg costs of CPLEX: ", round(mean(as.numeric(as.character(CPLEXCosts[["obj"]]))), digits = 2))
--- src/scripts/runtimes.R
+++ src/scripts/runtimes.R
@@ -1,14 +1,13 @@
 library(ggplot2)
 library(plyr)
 
-input <- read.csv(file = "res/solutions/solutions.csv", header = TRUE, sep = ",")
+input <- read.csv(file = "res/solutions/S77_solutions.csv", header = TRUE, sep = ",")
 
-solverEntries <- subset(input, solver == "LPT" | solver == "SPS")
+solverEntries <- subset(input, solver == "LPT" | solver == "SPS" | solver == "CPLEX")
 plotPointsPre <- ggplot(data = solverEntries, aes(x = as.numeric(as.character(runtime)), y = instance, color = solver, group = solver))
-plotPointsPreScaled <- plotPointsPre + geom_point() + xlab("runtime (s)") + ylab("instance")
-finalPlot <- plotPointsPreScaled #+ scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
+finalPlot <- plotPointsPre + geom_point() + xlab("runtime (s)") + ylab("instance")
 
-ggsave(finalPlot, file = "solver_instance_time.png", width = 6, height = 40)
+ggsave(finalPlot, file = "solver_instance_time.png", width = 6, height = 4)
 
 LPTData <- subset(input, solver == "LPT")
 LPTRuntime <- subset(LPTData, select = c(runtime))
@@ -17,3 +16,7 @@ paste("avg runtime of LPT: ", round(mean(as.numeric(as.character(LPTRuntime[["ru
 SPSData <- subset(input, solver == "SPS")
 SPSRuntime <- subset(SPSData, select = c(runtime))
 paste("avg runtime of SPS: ", round(mean(as.numeric(as.character(SPSRuntime[["runtime"]]))), digits = 2))
+
+CPLEXData <- subset(input, solver == "CPLEX")
+CPLEXRuntime <- subset(CPLEXData, select = c(runtime))
+paste("avg runtime of CPLEX: ", round(mean(as.numeric(as.character(CPLEXRuntime[["runtime"]]))), digits = 2))
