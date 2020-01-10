cfb9b85399f5c3b05dabf5d927a9f7757f59d62a
tbohne <tbohne@uos.de>
2020-01-10 18:17:12 +0100

    refined analysis scripts

--- src/scripts/costs.R
+++ src/scripts/costs.R
@@ -1,25 +1,19 @@
 library(ggplot2)
 
-input <- read.csv(file = "../../res/solutions/S60_solutions.csv", header = TRUE, sep = ",")
-solverEntries <- subset(input, solver == "LPT" | solver == "SPS" | solver == "CPLEX" | solver == "TS")
+input <- read.csv(file = "../../res/solutions/S70_solutions.csv", header = TRUE, sep = ",")
 
+solverEntries <- subset(input, solver == "LPT" | solver == "SPS" | solver == "CPLEX" | solver == "TS")
 plotPointsPre <- ggplot(data = solverEntries, aes(x = obj, y = instance, color = solver, group = solver))
 finalPlot <- plotPointsPre + geom_point() + xlab("costs") + ylab("instance")
-
 ggsave(finalPlot, file = "solver_instance_cost.png", width=6, height=4)
 
-LPTData <- subset(input, solver == "LPT")
-LPTCosts <- subset(LPTData, select = c(obj))
-paste("avg costs of LPT: ", round(mean(as.numeric(as.character(LPTCosts[["obj"]]))), digits = 2))
+compute_avg_costs <- function(s) {
+    data <- subset(input, solver == s)
+    costs <- subset(data, select = c(obj))
+    return(round(mean(as.numeric(as.character(costs[["obj"]]))), digits = 2))
+}
 
-SPSData <- subset(input, solver == "SPS")
-SPSCosts <- subset(SPSData, select = c(obj))
-paste("avg costs of SPS: ", round(mean(as.numeric(as.character(SPSCosts[["obj"]]))), digits = 2))
-
-CPLEXData <- subset(input, solver == "SPS")
-CPLEXCosts <- subset(CPLEXData, select = c(obj))
-paste("avg costs of CPLEX: ", round(mean(as.numeric(as.character(CPLEXCosts[["obj"]]))), digits = 2))
-
-TSData <- subset(input, solver == "TS")
-TSCosts <- subset(TSData, select = c(obj))
-paste("avg costs of TS: ", round(mean(as.numeric(as.character(TSCosts[["obj"]]))), digits = 2))
+paste("avg costs of LPT: ", compute_avg_costs("LPT"))
+paste("avg costs of SPS: ", compute_avg_costs("SPS"))
+paste("avg costs of CPLEX: ", compute_avg_costs("CPLEX"))
+paste("avg costs of TS: ", compute_avg_costs("TS"))
--- src/scripts/runtimes.R
+++ src/scripts/runtimes.R
@@ -1,26 +1,20 @@
 library(ggplot2)
 library(plyr)
 
-input <- read.csv(file = "../../res/solutions/S60_solutions.csv", header = TRUE, sep = ",")
+input <- read.csv(file = "../../res/solutions/S70_solutions.csv", header = TRUE, sep = ",")
 
 solverEntries <- subset(input, solver == "LPT" | solver == "SPS" | solver == "CPLEX" | solver == "TS")
 plotPointsPre <- ggplot(data = solverEntries, aes(x = as.numeric(as.character(runtime)), y = instance, color = solver, group = solver))
 finalPlot <- plotPointsPre + geom_point() + xlab("runtime (s)") + ylab("instance")
-
 ggsave(finalPlot, file = "solver_instance_time.png", width = 6, height = 4)
 
-LPTData <- subset(input, solver == "LPT")
-LPTRuntime <- subset(LPTData, select = c(runtime))
-paste("avg runtime of LPT: ", round(mean(as.numeric(as.character(LPTRuntime[["runtime"]]))), digits = 2))
+compute_avg_runtime <- function(s) {
+    data <- subset(input, solver == s)
+    runtime <- subset(data, select = c(runtime))
+    return(round(mean(as.numeric(as.character(runtime[["runtime"]]))), digits = 2))
+}
 
-SPSData <- subset(input, solver == "SPS")
-SPSRuntime <- subset(SPSData, select = c(runtime))
-paste("avg runtime of SPS: ", round(mean(as.numeric(as.character(SPSRuntime[["runtime"]]))), digits = 2))
-
-CPLEXData <- subset(input, solver == "CPLEX")
-CPLEXRuntime <- subset(CPLEXData, select = c(runtime))
-paste("avg runtime of CPLEX: ", round(mean(as.numeric(as.character(CPLEXRuntime[["runtime"]]))), digits = 2))
-
-TSData <- subset(input, solver == "TS")
-TSRuntime <- subset(TSData, select = c(runtime))
-paste("avg runtime of TS: ", round(mean(as.numeric(as.character(TSRuntime[["runtime"]]))), digits = 2))
+paste("avg runtime of LPT: ", compute_avg_runtime("LPT"))
+paste("avg runtime of SPS: ", compute_avg_runtime("SPS"))
+paste("avg runtime of CPLEX: ", compute_avg_runtime("CPLEX"))
+paste("avg runtime of TS: ", compute_avg_runtime("TS"))
